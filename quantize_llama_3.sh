31dcdd5d9e82b7ba0c07a1ea7d6720da059a9e46
catid <mrcatid@gmail.com>
2024-04-22 05:42:28 +0000

    wip

--- quantize_llama_3.sh
+++ quantize_llama_3.sh
@@ -5,13 +5,19 @@ HESS=hess
 BASE=Meta-Llama-3-8B-Instruct
 RESULT=llama3_8b_6144
 
+# 3 bits
+#CODEBOOK=E8P12RVQ3B
+
+# 4 bits
+CODEBOOK=E8P12RVQ4B
+
 mkdir -p $CKPT $HF $LOG $HESS
 
 # generate hessians (takes a while, only use this if there aren't pregenerated hessians)
 python hessian_offline_llama.py --batch_size 8 --devset_size 6144 --ctx_size 8192 --base_model $BASE --save_path $HESS/$RESULT
 
 # quantize with finetuning
-python -m quantize_llama.quantize_finetune_llama --save_path $CKPT/$RESULT --codebook E8P12  --scale_override 0.9 --base_model $BASE  --hessian_path $HESS/$RESULT/ --devset_size 384 --ft_valid_size 128 >> $LOG/$RESULT 2>&1
+python -m quantize_llama.quantize_finetune_llama --save_path $CKPT/$RESULT --codebook $CODEBOOK  --scale_override 0.9 --base_model $BASE  --hessian_path $HESS/$RESULT/ --devset_size 384 --ft_valid_size 128 >> $LOG/$RESULT 2>&1
 
 # convert model to hf format for end to end fine tuning
 python -m quantize_llama.hfize_llama --quantized_path $CKPT/$RESULT --hf_output_path $HF/$RESULT >> $LOG/$RESULT 2>&1
